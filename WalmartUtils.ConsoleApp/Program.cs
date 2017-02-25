7afd27170bfc3ceff0b75feea7a90522f3aefc7f
Andrey P <pfm@live.ru>
2017-02-25 15:07:31 +0300

    Write to file.

--- WalmartUtils.ConsoleApp/Program.cs
+++ WalmartUtils.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using Walmart.Entities.mp;
@@ -8,8 +9,16 @@ namespace WalmartUtils.ConsoleApp
 {
     class Program
     {
+        static string path = "prop-infos.txt";
+        private static StreamWriter sw;
+
+
         static void Main()
         {
+            if (File.Exists(path))
+                File.Delete(path);
+
+            sw = new StreamWriter(path);
 
             ShowPropInfos(typeof(Animal));
             ShowPropInfos(typeof(ArtAndCraft));
@@ -38,37 +47,54 @@ namespace WalmartUtils.ConsoleApp
             ShowPropInfos(typeof(Vehicle));
             ShowPropInfos(typeof(Watches));
 
+            sw.Close();
+            sw.Dispose();
+
             Console.ReadKey();
         }
 
         private static void ShowPropInfos(Type type)
         {
+
+
             foreach (var s in GetPropInfos(type))
             {
                 Console.WriteLine(s);
+                sw.WriteLine(s);
             }
+
         }
+
         public static IEnumerable<string> GetPropInfos(Type type)
         {
             foreach (var prop in type.GetProperties(BindingFlags.Public | BindingFlags.Instance))
             {
                 if (IsSimple(prop.PropertyType))
                 {
-                    yield return $"{$"{type.Name}/{prop.Name}",-65}{prop.PropertyType.Name}";
+                    yield return $"{$"{type.Name}/{prop.Name}",-85}{prop.PropertyType.Name}";
                 }
                 else if (prop.Name.ToLower() == "item")
                 {
-                    var attrs = prop.GetCustomAttributes<System.Xml.Serialization.XmlElementAttribute>().Select(a => a.Type);
+                    var attrs =
+                        prop.GetCustomAttributes<System.Xml.Serialization.XmlElementAttribute>().Select(a => a.Type);
                     foreach (var attr in attrs.Where(x => x != null))
                     {
                         foreach (var p in attr.GetProperties(BindingFlags.Public | BindingFlags.Instance))
                         {
                             if (IsSimple(p.PropertyType))
                             {
-                                yield return $"{$"{type.Name}/{attr.Name}/{p.Name}",-65}{p.PropertyType.Name}";
+                                yield return $"{$"{type.Name}/{attr.Name}/{p.Name}",-85}{p.PropertyType.Name}";
+                            }
+                            else // complex
+                            {
+                                yield return $"{$"{type.Name}/{attr.Name}/{p.Name}",-85}@complex";
                             }
                         }
                     }
+                }
+                else // complex
+                {
+                    yield return $"{$"{type.Name}/{prop.Name}",-85}@complex";
                 }
             }
         }
